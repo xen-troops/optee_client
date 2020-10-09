8a9d8bfc277441a25995d99a1c5b7c9ec4eca3be
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2020-10-09 18:08:46 +0300

    tee-supplicant: enable RPMB_EMU_PERSIST support for Android
    This fixes previous commit, which added RPMB_EMU_PERSIST support
    only to Makefile.
    
    Also, for Android RPMB_EMU_PERSIST is enabled by default.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- tee-supplicant/tee_supplicant_android.mk
+++ tee-supplicant/tee_supplicant_android.mk
@@ -31,9 +31,13 @@ LOCAL_CFLAGS += -DCFG_GP_SOCKETS=1
 endif
 
 RPMB_EMU        := 1
+RPMB_EMU_PERSIST ?= 1
 ifeq ($(RPMB_EMU),1)
 LOCAL_SRC_FILES += src/sha2.c src/hmac_sha2.c
 LOCAL_CFLAGS += -DRPMB_EMU=1
+ifeq ($(RPMB_EMU_PERSIST),1)
+LOCAL_CFLAGS += -DRPMB_EMU_PERSIST=1
+endif
 endif
 
 ifneq (,$(filter y,$(CFG_TA_GPROF_SUPPORT) $(CFG_FTRACE_SUPPORT)))
